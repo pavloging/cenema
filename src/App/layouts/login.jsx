f89cbe32d0b90ec296f5cd94b4a40321eea17bc5
Egor <ep9821123@gmail.com>
2022-07-08 09:28:16 +1000

    upgrade-login (html)

--- src/App/layouts/login.jsx
+++ src/App/layouts/login.jsx
@@ -3,6 +3,7 @@ import TextField from '../components/textField';
 import { validator } from '../untils/validator.js';
 
 const Login = () => {
+  const [login, setLogin] = useState(true);
   const [data, setData] = useState({ email: '', password: '' });
   const [errors, setErrors] = useState({});
   const handleChange = ({ target }) => {
@@ -57,7 +58,7 @@ const Login = () => {
     <div className="login">
       <div className="login__container">
         <div className="login__box">
-          <h3 className="login__title">Login</h3>
+          <h3 className="login__title">{login ? 'Login' : 'Register'}</h3>
           <form onSubmit={handleSubmit}>
             <TextField
               label="Email"
@@ -74,6 +75,57 @@ const Login = () => {
               onChange={handleChange}
               error={errors.password}
             />
+            {login ? null : (
+              <div className="register">
+                <div className="data mt-4 mb-3">
+                  <p>Your birthday:</p>
+                  <input type="date" name="date" id="date" />
+                </div>
+
+                <div className="login_register-radio mt-4 mb-3">
+                  <p className="login_register-paragraf mb-3">
+                    Choose your gender:
+                  </p>
+                  <div>
+                    <input
+                      className="form-check-input me-2"
+                      id="register-radio1"
+                      type="radio"
+                      name="radio"
+                    />
+                    <label htmlFor="register-radio1">Male</label>
+                  </div>
+                  <div>
+                    <input
+                      className="form-check-input me-2"
+                      id="register-radio2"
+                      type="radio"
+                      name="radio"
+                    />
+                    <label htmlFor="register-radio2">Female</label>
+                  </div>
+                  <div>
+                    <input
+                      className="form-check-input me-2"
+                      id="register-radio3"
+                      type="radio"
+                      name="radio"
+                    />
+                    <label htmlFor="register-radio3">Other</label>
+                  </div>
+                </div>
+                <div className="mt-4 mb-4">
+                  <label htmlFor="checkbox">
+                    <input type="checkbox" className="me-2" id="checkbox" />
+                    Confirm international agreement
+                  </label>
+                  <p>
+                    You may not use our service without confirming the license
+                    agreement
+                  </p>
+                </div>
+              </div>
+            )}
             <button
               className="btn btn-primary w-100 mx-auto"
               type="submit"
@@ -81,6 +133,17 @@ const Login = () => {
             >
               Submit
             </button>
+
+            <div className="login__sing-up d-flex mt-3">
+              <p className="login__dont-account mb-0">Dont have account?</p>
+              &ensp;
+              <p
+                onClick={() => setLogin(!login)}
+                className="login__sing-paragraf mb-0"
+              >
+                {login ? 'Sign Up' : 'Sign In'}
+              </p>
+            </div>
           </form>
         </div>
       </div>
