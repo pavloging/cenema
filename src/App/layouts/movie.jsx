7e24da8631f57a748453abe4ea822edda4f46628
Егор Павлов <egorpavlov@MacBook-Air-Egor.local>
2023-08-28 23:07:05 +0300

    fix design for mobile

--- src/App/layouts/movie.jsx
+++ src/App/layouts/movie.jsx
@@ -1,17 +1,24 @@
 const Movie = ({ film, youtube }) => {
   return (
     <div className="movie">
-      <h1 className="movie__title">{film.Title}</h1>
-      <img className="movie__img" src={film.Poster} alt="" />
-      <h4>Year: {film.Year}</h4>
-      <h4>Released: {film.Released}</h4>
-      <h4>Runtime: {film.Runtime}</h4>
-      <h4>Genre: {film.Genre}</h4>
-      <h4>Actors: {film.Actors}</h4>
-      <h4>Rate: {film.imdbRating}</h4>
-      <h5>Plot: {film.Plot}</h5>
+      <div className="movie__container">
+        <div className="movie__poster">
+          <h1 className="movie__title">{film.Title}</h1>
+          <img className="movie__img" src={film.Poster} alt="" />
+        </div>
+        <div className="movie__description">
+          <h4>Year: {film.Year}</h4>
+          <h4>Released: {film.Released}</h4>
+          <h4>Runtime: {film.Runtime}</h4>
+          <h4>Genre: {film.Genre}</h4>
+          <h4>Actors: {film.Actors}</h4>
+          <h4>Rate: {film.imdbRating}</h4>
+          <h5>Plot: {film.Plot}</h5>
+        </div>
+      </div>
       <iframe
-        width="90%"
+        className="movie__iframe"
+        width="100%"
         height="900px"
         src={youtube}
         title="YouTube video player"
